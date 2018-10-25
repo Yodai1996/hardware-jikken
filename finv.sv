c3ff35c9438e391f51873e6cefcf046e8ec6488e
taiyo3 <taiyoyodai@gmail.com>
2018-10-25 19:24:03 +0900

    Update finv.sv

--- finv.sv
+++ finv.sv
@@ -1,1 +1,34 @@
+`timescale 1ns / 1ps
+
+//when x<0, returns 1
+
+module finv(
+  input wire [31:0] x,
+  output wire [31:0] y);
+  
+  wire sx;
+  wire [7:0] ex;
+  wire [22:0] mx;
+  
+  assign {sx,ex,mx}=x;
+  
+  
+  
+  
+  
+  assign y = (x[31]==1 && x[30:23] !=8'b0) ? 1 : 0;
+
+endmodule   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
