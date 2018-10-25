d5187048baed5244cab2b31af4dda039af5d6f80
taiyo3 <taiyoyodai@gmail.com>
2018-10-25 23:07:48 +0900

    Update finv.sv

--- finv.sv
+++ finv.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-//when x<0, returns 1
+//とりあえず組み合わせ回路で実現しようか
 
 module finv(
   input wire [31:0] x,
@@ -12,11 +12,39 @@ module finv(
   
   assign {sx,ex,mx}=x;
   
+  //25bit_verに対応させた
+  wire [24:0] twice;
+  wire [24:0] square;
   
+  //テーブルをもとに作成
+  assign twice = 
+    () ? ..... :
+    () ? ..... : ...
   
+  assign square = ...
+    
+  wire [24:0] manx;
+  assign manx = {1'b1, mx, 1'b0};  
   
+  //same to [49:0], because b[49] is definetly 0
+  wire [48:0] b;
+  assign b = manx * square;
   
-  assign y = (x[31]==1 && x[30:23] !=8'b0) ? 1 : 0;
+  wire [24:0] m_inv;
+  assign m_inv = twice - b[48:24];  //ここでも切り捨ててるけど大丈夫かな？
+  
+  //基本的にm_invの最上位bitは0になり、次のbitが1になるはずであり、このとき残り23bitをか仮数部とすればよい。
+  //基本的にはstate==0なはず
+  wire state;
+  assign state = m_inv[24] ;
+  
+  wire [7:0] ey;
+  assign ey = state ? (8'd254 - ex) : (8'd253 - ex) ;
+  
+  wire [22:0] my;
+  assign my = state ? m_inv[23:1] : m_inv[22:0];
+  
+  assign y = {sx, ey,  my};
 
 endmodule   
    
